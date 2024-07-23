f0c03e16679b3cef7a0322d328e0f3a959b9f838
tulf <naseerr12t@gmail.com>
2024-07-23 09:39:48 +0300

    p1

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Validation\Rule;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(
+                request(['search', 'category', 'author'])
+            )->paginate(15)->withQueryString()
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+   
+}
